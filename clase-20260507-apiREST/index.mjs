5129e18b2fea6da86ee5e431b9a1c9e360a7ea9a
GarciaEzequiel <ezegarciaies@gmail.com>
2026-05-07 19:30:18 -0300

    Creamos apiREST

---
+++ clase-20260507-apiREST/index.mjs
@@ -0,0 +1,28 @@
+import express from 'express'
+import {} from './funciones.mjs'
+
+const PUERTO= 3000
+const app= express()
+
+// configurar una API REST basica
+
+//GET /api/v1/productos
+app.get('/api/v1/productos', (req, res)=>{
+    
+})
+
+// //GET /api/v1/productos/:id
+// app.get('/api/v1/productos/:id')
+
+// //POST /api/v1/productos
+// app.post('/api/v1/productos')
+
+// //PUT /api/v1/productos/:id
+// app.put('/api/v1/productos/:id')
+
+// //DELETE /api/v1/productos/:id
+// app.delete('/api/v1/productos/:id')
+
+
+
+app.listen(PUERTO)
---
+++ clase-20260507-apiREST/productos.mjs
@@ -0,0 +1,66 @@
+
+const productos = 
+[ 
+  {
+    "id": 1,
+    "nombre": "Martillo Carpintero",
+    "precio": 12500,
+    "imagen": "https://images.unsplash.com/photo-1504148455328-c376907d081c"
+  },
+  {
+    "id": 2,
+    "nombre": "Taladro Eléctrico",
+    "precio": 68500,
+    "imagen": "https://images.unsplash.com/photo-1504307651254-35680f356dfd"
+  },
+  {
+    "id": 3,
+    "nombre": "Juego de Destornilladores",
+    "precio": 9800,
+    "imagen": "https://images.unsplash.com/photo-1581147036324-c1c9f4049f22"
+  },
+  {
+    "id": 4,
+    "nombre": "Llave Francesa",
+    "precio": 15400,
+    "imagen": "https://images.unsplash.com/photo-1609205807107-e8ec2120f8f2"
+  },
+  {
+    "id": 5,
+    "nombre": "Caja de Tornillos",
+    "precio": 4500,
+    "imagen": "https://images.unsplash.com/photo-1517048676732-d65bc937f952"
+  },
+  {
+    "id": 6,
+    "nombre": "Sierra Circular",
+    "precio": 89400,
+    "imagen": "https://images.unsplash.com/photo-1517048676732-d65bc937f952"
+  },
+  {
+    "id": 7,
+    "nombre": "Cinta Métrica",
+    "precio": 3200,
+    "imagen": "https://images.unsplash.com/photo-1581092921461-eab62e97a780"
+  },
+  {
+    "id": 8,
+    "nombre": "Pinza Universal",
+    "precio": 7600,
+    "imagen": "https://images.unsplash.com/photo-1530124566582-a618bc2615dc"
+  },
+  {
+    "id": 9,
+    "nombre": "Pala Punta Cuadrada",
+    "precio": 18700,
+    "imagen": "https://images.unsplash.com/photo-1597424216808-8c5a0a3f1f33"
+  },
+  {
+    "id": 10,
+    "nombre": "Amoladora Angular",
+    "precio": 73200,
+    "imagen": "https://images.unsplash.com/photo-1509395176047-4a66953fd231"
+  }
+]
+
+export default productos 
